30bcee0ea915e001742503c72a62c5657d513b33
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2019-03-13 13:07:44 +0100

    Revert "update: changed the var keyword to let"
    This reverts commit 540da086b9ffed68eca8725e5e925b26ca5b4657.

--- fibonacci.js
+++ fibonacci.js
@@ -1,11 +1,11 @@
 self.onmessage = function(e) {
-  let userNum = Number(e.data);
+  var userNum = Number(e.data);
   fibonacci(userNum);
 }
 
 
 function fibonacci(num){
-let a = 1, b = 0, temp;
+var a = 1, b = 0, temp;
   while (num >= 0){
     temp = a;
     a = a + b;
