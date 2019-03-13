928c17ac9f6c13297c59e877346d8e322fd1f70b
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2019-03-13 13:07:49 +0100

    Revert "simplifying and improving fibonacci generation"
    This reverts commit c9b63c601c348f11bd5fc2e2f732ea2b83595cc4.

--- fibonacci.js
+++ fibonacci.js
@@ -1,17 +1,28 @@
-self.onmessage = function(e) {
-  var userNum = Number(e.data);
-  fibonacci(userNum);
+var results = [];
+
+function resultReceiver(event) {
+  results.push(parseInt(event.data));
+  if (results.length == 2) {
+    postMessage(results[0] + results[1]);
+  }
 }
 
+function errorReceiver(event) {
+  throw event.data;
+}
 
-function fibonacci(num){
-var a = 1, b = 0, temp;
-  while (num >= 0){
-    temp = a;
-    a = a + b;
-    b = temp;
-    num--;
+onmessage = function(event) {
+  var n = parseInt(event.data);
+
+  if (n == 0 || n == 1) {
+    postMessage(n);
+    return;
   }
 
-  self.postMessage(b);
-}
+  for (var i = 1; i <= 2; i++) {
+    var worker = new Worker('fibonacci.js');
+    worker.onmessage = resultReceiver;
+    worker.onerror = errorReceiver;
+    worker.postMessage(n - i);
+  }
+};
